c78c01dc1da7634629967787f849a75c51233df8
askmary <mariana.reinaldo2003@gmail.com>
2022-02-04 20:21:14 -0300

    challenge 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import farofa from "./farofa.jpg"
+class dados extends Component{
+  state = {
+    nome: "Mariana",
+    idade: 18,
+    comida: "farofa",
+    musicas: ["Tive Razão", "Bohemian Rhapsody", "Bonekinha"]
+  }
+  render(){
+    return(
+      <div>
+        <h1>Meu nome é {this.state.nome}</h1>
+        <h2>Minha idade é {this.state.idade}</h2>
+        <h3>Minha comida favorita é {this.state.comida}</h3>
+        <ul>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ul>
+        <img src={farofa}/>
+        <p>NAO COMO FRUTA</p>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default dados
